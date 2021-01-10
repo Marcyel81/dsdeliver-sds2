9ce20e01616be7daaf1aebf8d5a4a9c6e2a0cb20
Mayara Martins Pessoa <marcyel81@gmail.com>
2021-01-10 12:07:57 -0300

    Format ProductCards Img_Price

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,18 +1,25 @@
 import './styles.css';
 import StepsHeader from './StepsHeader';
 import ProductsList from './ProductsList';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from './api';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
 
     useEffect(() => {
-        console.log('component orders iniciou')
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+
     }, []);
 
     return(
         <div className="orders-container">
             <StepsHeader />
-            <ProductsList />
+            <ProductsList products={products}/>
         </div>
     )
 }
